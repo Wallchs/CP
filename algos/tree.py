4a574d07a9760a2c560603e70cec6e8581a7b157
wallchs <wallace.s.chagas@gmail.com>
2024-03-01 04:26:00 -0300

    incluindo trees

---
+++ algos/tree.py
@@ -0,0 +1,3 @@
+#A árvore é a mesma coisa da linked list pois é! Como eu nunca pensei nisso antes.
+#a diferença é que ela tem duas direções a mais no seu node left e right o restante do código é parecido 
+#os elementos são chamados de parent(pais) e após os galhos são chamados de childNodes(galhos/filhos), que também pode ter ramificações os chamados leaf(folhas)
